3fa5e1143d16128fb17bcde8a9c4be1c7cc2f07b
Harshita Phadtare <hphadtare2023@gmail.com>
2024-05-01 00:15:52 +0530

    added more files

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ballotify</title>
+    <link rel="stylesheet" href="./style/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="input-group">
+        <div class="input-field">
+            <input type="text" id="" name="name">
+            <label class="labelLine" for="name">Full Name</label>
+        </div>
+    </div>
+<!-- 
+    <div class="backdrop"> -->
+        
+        
+        <!-- <div class="left">
+            <div class="logo">
+                <img height="70px" width="70px" src="assets/raise-hand.png" alt="voting hands raised">
+                <h1 class="h1">Ballotify</h1>
+                <div class="underline"></div>
+            </div>
+            <p class="details">
+                Join the movement for democratic engagement,where your voice matters and your vote makes a difference. Together, let's shape a brighter tomorrow through the power of digital democracy.
+            </p>
+        </div>
+        <div class="right">
+            <div class="form">
+                <h1>Sign Up</h1>
+                <form action="">
+                    <div class="input-group">
+                        <div class="input-field">
+                            <input type="text" required id="">
+                            <div class="labelLine">Full Name</div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div> -->
+    <!-- </div> -->
+
+    
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
