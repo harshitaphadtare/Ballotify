d75f51434d352740e4b5d58ca198b181b0538112
Harshita Phadtare <hphadtare2023@gmail.com>
2024-05-05 21:55:37 +0530

    admin page completed

--- admin/inc/candidate.php
+++ admin/inc/candidate.php
@@ -2,7 +2,7 @@
     if(isset($_GET['added'])){
     ?>
         <div class="alert alert-success alert-dismissible fade show d-flex justify-content-between" role="alert">
-            Election has been added Successfully!
+            Candidate has been added Successfully!
             <button type="button" class="close border bg-transparent" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
@@ -10,11 +10,73 @@
         <script>
             document.querySelector(".close").addEventListener('click', () => {
                 document.querySelector(".alert").style.visibility = 'hidden';
-                window.location.href = 'index.php?addElectionPage=1';
+                window.location.href = 'index.php?addCandidatePage=1';
             });
         </script>
     <?php
+    }else if(isset($_GET['largeFile'])){
+    ?>
+        <div class="alert alert-warning alert-dismissible fade show d-flex justify-content-between" role="alert">
+            Image is too Large, please upload image that has size less than 2MB!
+            <button type="button" class="close border bg-transparent" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <script>
+            document.querySelector(".close").addEventListener('click', () => {
+                document.querySelector(".alert").style.visibility = 'hidden';
+                window.location.href = 'index.php?addCandidatePage=1';
+            });
+        </script>
+    <?php 
+    }else if(isset($_GET['invalidFile'])){
+        ?>
+            <div class="alert alert-danger alert-dismissible fade show d-flex justify-content-between" role="alert">
+                Invalid Image type: only .jpg, .jpeg and .png formated files are allowed!
+                <button type="button" class="close border bg-transparent" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <script>
+                document.querySelector(".close").addEventListener('click', () => {
+                    document.querySelector(".alert").style.visibility = 'hidden';
+                    window.location.href = 'index.php?addCandidatePage=1';
+                });
+            </script>
+        <?php
+    }else if(isset($_GET['failed'])){
+        ?>
+            <div class="alert alert-danger alert-dismissible fade show d-flex justify-content-between" role="alert">
+                Image uploading failed, please try again.
+                <button type="button" class="close border bg-transparent" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <script>
+                document.querySelector(".close").addEventListener('click', () => {
+                    document.querySelector(".alert").style.visibility = 'hidden';
+                    window.location.href = 'index.php?addCandidatePage=1';
+                });
+            </script>
+        <?php
+    }else if(isset($_GET['added'])){
+        ?>
+            <div class="alert alert-success alert-dismissible fade show d-flex justify-content-between" role="alert">
+                Candidate details have been added Successfully!
+                <button type="button" class="close border bg-transparent" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <script>
+                document.querySelector(".close").addEventListener('click', () => {
+                    document.querySelector(".alert").style.visibility = 'hidden';
+                    window.location.href = 'index.php?addCandidatePage=1';
+                });
+            </script>
+        <?php
     }
+
+
 ?>
 
 
@@ -35,6 +97,12 @@
     .form-group{
         margin-bottom: 15px;
     }
+    .candidate-photo{
+        width: 80px;
+        height: 80px;
+        border: 2px solid #58B19F;
+        border-radius: 50%;
+    }
 </style>
 <body>
 
@@ -53,9 +121,17 @@
 
                                     $election_id = $row['id'];
                                     $election_name = $row['election_topic'];
-                                    ?>
-                                    <option value="<?php echo $election_id; ?>"> <?php echo $election_name; ?></option>
-                                    <?php
+                                    $allowed_candidates = $row['no_of_candidates'];
+
+                                    //checking number of candidates added in en election
+                                    $fetchingCandidate = mysqli_query($db,"SELECT * FROM candidate_details WHERE election_id ='".$election_id."' ") or die(mysqli_error($db));
+                                    $added_candidates = mysqli_num_rows($fetchingCandidate);
+
+                                    if($added_candidates < $allowed_candidates){
+                                        ?>
+                                        <option value="<?php echo $election_id; ?>"> <?php echo $election_name; ?></option>
+                                        <?php
+                                    }
                                 }
                             }else{
                                 ?>
@@ -83,31 +159,37 @@
                 <thead>
                     <tr>
                         <th scope="col">S.No</th>
-                        <th scope="col">Election Name</th>
-                        <th scope="col">Candidate</th>
-                        <th scope="col">Starting Date</th>
-                        <th scope="col">Ending Date</th>
-                        <th scope="col">Status</th>
+                        <th scope="col">Photo</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Details</th>
+                        <th scope="col">Election</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                        $fetchingData = mysqli_query($db,"SELECT * FROM elections") or die(mysqli_error($db));
+                        $fetchingData = mysqli_query($db,"SELECT * FROM candidate_details") or die(mysqli_error($db));
 
-                        $isAnyElectionAdded = mysqli_num_rows($fetchingData);
+                        $isAnyCandidateAdded = mysqli_num_rows($fetchingData);
 
-                        if($isAnyElectionAdded > 0){
+                        if($isAnyCandidateAdded > 0){
                             $sno=1;
                             while($row = mysqli_fetch_assoc($fetchingData)){
+                                
+                                $election_id = $row['election_id'];
+                                $fetchingElectionName = mysqli_query($db,"SELECT * FROM elections WHERE id = '".$election_id."'") or die(mysqli_error($db));
+                                $execFetchingElectionNameQuery = mysqli_fetch_assoc($fetchingElectionName) ;
+                                $election_name = $execFetchingElectionNameQuery['election_topic'];
+
+                                $candidate_photo = $row['candidate_photo'];
+
                                 ?>
-                                <tr>
+                                <tr class="align-middle">
                                     <td><?php echo $sno++; ?></td>
-                                    <td><?php echo $row["election_topic"]; ?></td>
-                                    <td><?php echo $row["no_of_candidates"]; ?></td>
-                                    <td><?php echo $row["starting_date"]; ?></td>
-                                    <td><?php echo $row["ending_date"]; ?></td>
-                                    <td><?php echo $row["status"]; ?></td>
+                                    <td><img src="<?php echo $candidate_photo; ?>" alt="candidate photo" class="candidate-photo"></td>
+                                    <td><?php echo $row["candidate_name"]; ?></td>
+                                    <td><?php echo $row["candidate_details"]; ?></td>
+                                    <td><?php echo $election_name; ?></td>
                                     <td>
                                         <a href="#" class="btn btn-warning btn-sm">Edit</a>
                                         <a href="#" class="btn btn-danger btn-sm">Delete</a>
@@ -118,7 +200,7 @@
                         }else{
                             ?>
                                 <tr>
-                                    <td colspan="7"> No Elections are added yet.</td>
+                                    <td colspan="7"> No Candidates are added yet.</td>
                                 </tr>
                             <?php
                         }
@@ -135,24 +217,41 @@
        
     if(isset($_POST['addCandidateBtn']))
     {
-        //               
-
+        
         $election_id = mysqli_real_escape_string($db,$_POST['election_id']);
         $candidate_name = mysqli_real_escape_string($db,$_POST['candidate_name']);
         $candidate_details = mysqli_real_escape_string($db,$_POST['candidate_details']);
         $inserted_by = $_SESSION['fullName'];
         $inserted_on = date('Y-m-d');
 
-        $candidate_photo = mysqli_real_escape_string($db,$_POST['candidate_photo']);
+        // photo logic
+        $targetted_photo ='../assets/candidate_photo/';
+        $candidate_photo = $targetted_photo . rand(1111111111,9999999999999) . "_" . rand(1111111111,9999999999999) .  $_FILES['candidate_photo']['name'];
+        $candidate_photo_tmp_name = $_FILES['candidate_photo']['tmp_name'];
+        $candidate_photo_type = strtolower(pathinfo($candidate_photo,PATHINFO_EXTENSION));
+        $allowed_types = array("jpg","png","jpeg");
+        $image_size = $_FILES['candidate_photo']['size'];
 
+        if($image_size < 2000000){ //2MB
+
+            if(in_array($candidate_photo_type,$allowed_types)){
+                if(move_uploaded_file($candidate_photo_tmp_name,$candidate_photo)){
+
+                    mysqli_query($db,"INSERT INTO candidate_details(election_id,candidate_name,candidate_details,candidate_photo,inserted_by,inserted_on) VALUES('".$election_id."','".$candidate_name."','".$candidate_details."','".$candidate_photo."','".$inserted_by."','".$inserted_on."')") or die(mysqli_error($db));
+
+                    echo "<script> location.assign('index.php?addCandidatePage=1&added=1'); </script>";
+
+                }else{
+                    echo "<script> location.assign('index.php?addCandidatePage=1&failed=1'); </script>";
+                }
+            }else{
+                echo "<script> location.assign('index.php?addCandidatePage=1&invalidFile=1'); </script>";
+            }
+
+        }else{
+            echo "<script> location.assign('index.php?addCandidatePage=1&largeFile=1'); </script>";
+        }
         
-
-        //inserting into db
-        mysqli_query($db,"INSERT INTO elections(election_topic,no_of_candidates,starting_date,ending_date,status,inserted_by,inserted_on) VALUES('".$election_topic."','".$no_of_candidates."','".$starting_date."','".$ending_date."','".$status."','".$inserted_by."','".$inserted_on."')") or die(mysqli_error($db));
-
-        ?>
-            <script>location.assign('index.php?addElectionPage=1&added=1')</script>
-        <?php
 
     } 
 
--- admin/inc/home.php
+++ admin/inc/home.php
@@ -1,1 +1,63 @@
-homePage...
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+
+    <div class="row m-3">
+        <div class="col-12">
+            <h4 class="h4 mb-4">Elections</h4>
+            <table class="table table-hover border-top">
+                <thead>
+                    <tr>
+                        <th scope="col">S.No</th>
+                        <th scope="col">Election Name</th>
+                        <th scope="col">Candidate</th>
+                        <th scope="col">Starting Date</th>
+                        <th scope="col">Ending Date</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $fetchingData = mysqli_query($db,"SELECT * FROM elections") or die(mysqli_error($db));
+
+                        $isAnyElectionAdded = mysqli_num_rows($fetchingData);
+
+                        if($isAnyElectionAdded > 0){
+                            $sno=1;
+                            while($row = mysqli_fetch_assoc($fetchingData)){
+                                ?>
+                                <tr>
+                                    <td><?php echo $sno++; ?></td>
+                                    <td><?php echo $row["election_topic"]; ?></td>
+                                    <td><?php echo $row["no_of_candidates"]; ?></td>
+                                    <td><?php echo $row["starting_date"]; ?></td>
+                                    <td><?php echo $row["ending_date"]; ?></td>
+                                    <td><?php echo $row["status"]; ?></td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-success">View Results</a>
+                                    </td>
+                                </tr>
+                                <?php
+                            };
+                        }else{
+                            ?>
+                                <tr>
+                                    <td colspan="7"> No Elections are added yet.</td>
+                                </tr>
+                            <?php
+                        }
+                    ?>
+                </tbody>
+                </table>
+        </div>
+    </div>
+
+</body>
+</html>
+
